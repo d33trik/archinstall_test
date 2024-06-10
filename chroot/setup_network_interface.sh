5a57139a4e7e7cf3d7faad5ece13bf39ba1b522c
Claudio Dietrich <claudio@dietrich.dev.br>
2024-06-10 19:25:04 -0300

    Commit

--- chroot/setup_network_interface.sh
+++ chroot/setup_network_interface.sh
@@ -1,8 +1,7 @@
 hostname=${1:?}
 
 echo "$hostname" > /etc/hostname
-pacman -S --noconfirm --needed networkmanager iptables-nft ufw gufw
-# yes | pacman -S --noconfirm --needed networkmanager iptables-nft ufw gufw
+yes | pacman -S --noconfirm --needed networkmanager iptables-nft ufw gufw
 systemctl enable NetworkManager.service
 systemctl enable ufw.service
 systemctl start ufw.service
