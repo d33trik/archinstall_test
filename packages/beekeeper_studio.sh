7049a164b29d3545ff885a6b90cb80094eb086bb
Claudio Dietrich <claudio@dietrich.dev.br>
2024-06-10 22:43:53 -0300

    Commit

--- packages/beekeeper_studio.sh
+++ packages/beekeeper_studio.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --noconfirm --needed fuse2
-curl -sLo beekeeper-studio.AppImage $(curl -s https://api.github.com/repos/beekeeper-studio/beekeeper-studio/releases/latest | grep browser_download_url.*AppImage | grep -v "arm64" | cut -d" -f4)
+curl -sLo beekeeper-studio.AppImage $(curl -s https://api.github.com/repos/beekeeper-studio/beekeeper-studio/releases/latest | grep browser_download_url.*AppImage | grep -v "arm64" | cut -d\" -f4)
 sudo mkdir /opt/beekeeper-studio
 sudo mv beekeeper-studio.AppImage /opt/beekeeper-studio
 sudo chmod +x /opt/beekeeper-studio/beekeeper-studio.AppImage
