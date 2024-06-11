a7868c402ae814a28873b0090e0d06968743b2ae
Claudio Dietrich <claudio@dietrich.dev.br>
2024-06-10 23:47:47 -0300

    Commit

--- packages/obsidian.sh
+++ packages/obsidian.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --noconfirm --needed fuse2
-curl -sLo obsidian.AppImage $(curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep browser_download_url.*AppImage | grep -v "arm64" | cut -d\" -f4)
+sudo curl -sLo obsidian.AppImage $(curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep browser_download_url.*AppImage | grep -v "arm64" | cut -d\" -f4)
 sudo mkdir /opt/obsidian
 sudo mv obsidian.AppImage /opt/obsidian
 sudo chmod +x /opt/obsidian/obsidian.AppImage
