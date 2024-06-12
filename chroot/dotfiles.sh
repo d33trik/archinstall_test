a517b61daeab1bb341baa47dd1145fe41fdba714
Claudio Dietrich <claudio@dietrich.dev.br>
2024-06-12 00:39:09 -0300

    Commit

--- chroot/dotfiles.sh
+++ chroot/dotfiles.sh
@@ -2,4 +2,5 @@ sudo pacman -S --noconfirm --needed git
 git clone https://github.com/d33trik/dotfiles.git "$HOME"/dotfiles
 cd "$HOME"/dotfiles
 chmod u+x install.sh
+mkdir -p "$HOME/.config"
 bash install.sh
