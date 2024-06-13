3bd0b0b0e8aa960088f9b8b771bccefa09c22b03
Claudio Dietrich <claudio@dietrich.dev.br>
2024-06-13 00:45:32 -0300

    Commit

--- chroot/setup_graphical_interface.sh
+++ chroot/setup_graphical_interface.sh
@@ -1,6 +1,6 @@
 keymap=${1:?}
 
-pacman -S --noconfirm --needed xorg xorg-xinit i3-wm i3status i3lock arandr dmenu feh arc-gtk-theme arc-icon-theme breeze
+pacman -S --noconfirm --needed xorg xorg-xinit i3-wm i3status i3lock arandr dmenu feh arc-gtk-theme arc-icon-theme breeze dunst libnotify
 mkdir -p /etc/X11/xorg.conf.d
 cat <<EOF > /etc/X11/xorg.conf.d/00-keyboard.conf
 Section "InputClass"
